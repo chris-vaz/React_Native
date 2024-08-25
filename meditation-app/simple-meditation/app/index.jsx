5e3762d6e31903eeee65e6a2d38aa1204ae3c667
chris-vaz <chrisvas2001@gmail.com>
2024-08-25 14:33:41 +0300

    Updated assets and setup dev env

---
+++ meditation-app/simple-meditation/app/index.jsx
@@ -0,0 +1,23 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#000000',
+  },
+});
+
+
