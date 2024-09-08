d1ecbe24bf7a62e818c955649fca34909cee69eb
chris-vaz <chrisvas2001@gmail.com>
2024-09-08 15:47:11 +0300

    Updated GuidedAffirmationsGallery component

---
+++ meditation-app/simple-meditation/components/GuidedAffirmationsGallery.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+interface GuidedAffirmationsGalleryProps{
+    title: string;
+}
+
+const GuidedAffirmationsGallery = () => {
+    return (
+        <View>
+            <Text>GuidedAffirmationsGallery</Text>
+        </View>
+    )
+}
+
+export default GuidedAffirmationsGallery
